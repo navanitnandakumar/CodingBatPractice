287c995f29e1e598e77a454ef2b074876ecdadc8
Navanit Nandakumar <navanitnandakumar@gmail.com>
2020-04-25 19:06:20 +0530

    Update Warmup2 - String_match.py

--- Warmup2 - String_match.py
+++ Warmup2 - String_match.py
@@ -7,3 +7,15 @@ def string_match(a, b):
     if a_sub == b_sub:
       count = count + 1
   return count
+
+#Expected	Run		
+#string_match('xxcaazz', 'xxbaaz') → 3	3	OK	
+#string_match('abc', 'abc') → 2	2	OK	
+#string_match('abc', 'axc') → 0	0	OK	
+#string_match('hello', 'he') → 1	1	OK	
+#string_match('he', 'hello') → 1	1	OK	
+#string_match('h', 'hello') → 0	0	OK	
+#string_match('', 'hello') → 0	0	OK	
+#string_match('aabbccdd', 'abbbxxd') → 1	1	OK	
+#string_match('aaxxaaxx', 'iaxxai') → 3	3	OK	
+#string_match('iaxxai', 'aaxxaaxx') → 3	3	OK
