197222571e82dd9cdf9875fece4e7af28e4fc776
Navanit Nandakumar <navanitnandakumar@gmail.com>
2020-04-27 19:33:01 +0530

    Create String1 - Hello_name.py

---
+++ String1 - Hello_name.py
@@ -0,0 +1,17 @@
+def hello_name(name):
+  output = "Hello "+name+"!"
+  return output
+  
+#Expected	Run		
+#hello_name('Bob') → 'Hello Bob!'	'Hello Bob!'	OK	
+#hello_name('Alice') → 'Hello Alice!'	'Hello Alice!'	OK	
+#hello_name('X') → 'Hello X!'	'Hello X!'	OK	
+#hello_name('Dolly') → 'Hello Dolly!'	'Hello Dolly!'	OK	
+#hello_name('Alpha') → 'Hello Alpha!'	'Hello Alpha!'	OK	
+#hello_name('Omega') → 'Hello Omega!'	'Hello Omega!'	OK	
+#hello_name('Goodbye') → 'Hello Goodbye!'	'Hello Goodbye!'	OK	
+#hello_name('ho ho ho') → 'Hello ho ho ho!'	'Hello ho ho ho!'	OK	
+#hello_name('xyz!') → 'Hello xyz!!'	'Hello xyz!!'	OK	
+#hello_name('Hello') → 'Hello Hello!'	'Hello Hello!'	OK	
+#other tests
+#OK
