66a6ed4b5e710230e57e64a1f290a3bb2a411a1c
Navanit Nandakumar <navanitnandakumar@gmail.com>
2020-04-30 09:34:54 +0530

    Create String1 - Extra_end.py

---
+++ String1 - Extra_end.py
@@ -0,0 +1,11 @@
+def extra_end(str):
+  return(str[-2:]+str[-2:]+str[-2:])
+ 
+#Expected	Run		
+#extra_end('Hello') → 'lololo'	'lololo'	OK	
+#extra_end('ab') → 'ababab'	'ababab'	OK	
+#extra_end('Hi') → 'HiHiHi'	'HiHiHi'	OK	
+#extra_end('Candy') → 'dydydy'	'dydydy'	OK	
+#extra_end('Code') → 'dedede'	'dedede'	OK	
+#other tests
+#OK	
