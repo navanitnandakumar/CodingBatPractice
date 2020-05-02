1f70e4374ef5f09519915afe2f8f4d53a36467bb
Navanit Nandakumar <navanitnandakumar@gmail.com>
2020-05-02 08:56:36 +0530

    Create String1 - First_two.py

---
+++ String1 - First_two.py
@@ -0,0 +1,14 @@
+def first_two(str):
+  return str[:2]
+
+#Expected	Run		
+#first_two('Hello') → 'He'	'He'	OK	
+#first_two('abcdefg') → 'ab'	'ab'	OK	
+#first_two('ab') → 'ab'	'ab'	OK	
+#first_two('a') → 'a'	'a'	OK	
+#first_two('') → ''	''	OK	
+#first_two('Kitten') → 'Ki'	'Ki'	OK	
+#first_two('hi') → 'hi'	'hi'	OK	
+#first_two('hiya') → 'hi'	'hi'	OK	
+#other tests
+#OK	
