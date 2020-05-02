923db5a3d059309e917b6290e89949c634082194
Navanit Nandakumar <navanitnandakumar@gmail.com>
2020-05-02 09:06:38 +0530

    Create String1 - Without_end.py

---
+++ String1 - Without_end.py
@@ -0,0 +1,14 @@
+def without_end(str):
+  return str[1:-1]
+
+#Expected	Run		
+#without_end('Hello') → 'ell'	'ell'	OK	
+#without_end('java') → 'av'	'av'	OK	
+#without_end('coding') → 'odin'	'odin'	OK	
+#without_end('code') → 'od'	'od'	OK	
+#without_end('ab') → ''	''	OK	
+#without_end('Chocolate!') → 'hocolate'	'hocolate'	OK	
+#without_end('kitten') → 'itte'	'itte'	OK	
+#without_end('woohoo') → 'ooho'	'ooho'	OK	
+#other tests
+#OK	
