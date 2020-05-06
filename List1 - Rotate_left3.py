cdad4d8d31681733b36e65cd854d63237cd89c8a
Navanit Nandakumar <navanitnandakumar@gmail.com>
2020-05-06 21:00:38 +0530

    Create List1 - Rotate_left3.py

---
+++ List1 - Rotate_left3.py
@@ -0,0 +1,11 @@
+def rotate_left3(nums):
+  return [nums[1], nums[2], nums[0]]
+
+#Expected	Run		
+#rotate_left3([1, 2, 3]) → [2, 3, 1]	[2, 3, 1]	OK	
+#rotate_left3([5, 11, 9]) → [11, 9, 5]	[11, 9, 5]	OK	
+#rotate_left3([7, 0, 0]) → [0, 0, 7]	[0, 0, 7]	OK	
+#rotate_left3([1, 2, 1]) → [2, 1, 1]	[2, 1, 1]	OK	
+#rotate_left3([0, 0, 1]) → [0, 1, 0]	[0, 1, 0]	OK	
+#other tests
+#OK	
