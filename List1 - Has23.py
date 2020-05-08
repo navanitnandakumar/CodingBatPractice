28d8b502724e6fe6d5961bb19b2630206c0fb661
Navanit Nandakumar <navanitnandakumar@gmail.com>
2020-05-08 20:39:30 +0530

    Create List1 - Has23.py

---
+++ List1 - Has23.py
@@ -0,0 +1,15 @@
+def has23(nums):
+  return 2 in nums or 3 in nums
+
+#Expected	Run		
+#has23([2, 5]) → True	True	OK	
+#has23([4, 3]) → True	True	OK	
+#has23([4, 5]) → False	False	OK	
+#has23([2, 2]) → True	True	OK	
+#has23([3, 2]) → True	True	OK	
+#has23([3, 3]) → True	True	OK	
+#has23([7, 7]) → False	False	OK	
+#has23([3, 9]) → True	True	OK	
+#has23([9, 5]) → False	False	OK	
+#other tests
+#OK	
