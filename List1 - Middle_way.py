983c18bfe04728c70ed27727adf1583e74977bb7
Navanit Nandakumar <navanitnandakumar@gmail.com>
2020-05-08 20:29:33 +0530

    Create List1 - Middle_way.py

---
+++ List1 - Middle_way.py
@@ -0,0 +1,12 @@
+def middle_way(a, b):
+  return [a[1] , b[1]]
+  
+#Expected	Run		
+#middle_way([1, 2, 3], [4, 5, 6]) → [2, 5]	[2, 5]	OK	
+#middle_way([7, 7, 7], [3, 8, 0]) → [7, 8]	[7, 8]	OK	
+#middle_way([5, 2, 9], [1, 4, 5]) → [2, 4]	[2, 4]	OK	
+#middle_way([1, 9, 7], [4, 8, 8]) → [9, 8]	[9, 8]	OK	
+#middle_way([1, 2, 3], [3, 1, 4]) → [2, 1]	[2, 1]	OK	
+#middle_way([1, 2, 3], [4, 1, 1]) → [2, 1]	[2, 1]	OK	
+#other tests
+#OK
